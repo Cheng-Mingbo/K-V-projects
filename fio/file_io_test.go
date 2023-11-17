f31fcaadbd9c56128bab4f9f8715f4ce524fe993
ChengMingbo <cheng@ChengMingbodeMacBook-Pro.local>
2023-11-17 16:57:55 +0800

    完成数据库启动流程

--- fio/file_io_test.go
+++ fio/file_io_test.go
@@ -1,1 +1,109 @@
 package fio
+
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func deleteFile(path string) {
+	if err := os.Remove(path); err != nil {
+		panic(err)
+	}
+}
+
+func TestNewFileIOManger(t *testing.T) {
+	path := filepath.Join("/tmp", "a.data")
+	fio, err := NewFileIOManger(path)
+	defer deleteFile(path)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, fio)
+}
+
+func TestFileIO_Read(t *testing.T) {
+	path := filepath.Join("/tmp", "a.data")
+	fio, err := NewFileIOManger(path)
+	defer deleteFile(path)
+	assert.Nil(t, err)
+	assert.NotNil(t, fio)
+
+	n, err := fio.Write([]byte("hello"))
+	assert.Equal(t, 5, n)
+	assert.Nil(t, err)
+
+	n, err = fio.Write([]byte("world"))
+	assert.Equal(t, 5, n)
+	assert.Nil(t, err)
+
+	b := make([]byte, 5)
+	n, err = fio.Read(b, 0)
+	assert.Equal(t, 5, n)
+	assert.Nil(t, err)
+	assert.Equal(t, "hello", string(b))
+
+	b1 := make([]byte, 5)
+	n, err = fio.Read(b1, 5)
+	assert.Equal(t, 5, n)
+	assert.Nil(t, err)
+	assert.Equal(t, "world", string(b1))
+
+}
+
+func TestFileIO_Write(t *testing.T) {
+	path := filepath.Join("/tmp", "a.data")
+	fio, err := NewFileIOManger(path)
+	defer deleteFile(path)
+	assert.Nil(t, err)
+	assert.NotNil(t, fio)
+
+	n, err := fio.Write([]byte("hello"))
+	assert.Equal(t, 5, n)
+	assert.Nil(t, err)
+
+	n, err = fio.Write([]byte("world"))
+	assert.Equal(t, 5, n)
+	assert.Nil(t, err)
+}
+
+func TestFileIO_Sync(t *testing.T) {
+	path := filepath.Join("/tmp", "a.data")
+	fio, err := NewFileIOManger(path)
+	defer deleteFile(path)
+	assert.Nil(t, err)
+	assert.NotNil(t, fio)
+
+	n, err := fio.Write([]byte("hello"))
+	assert.Equal(t, 5, n)
+	assert.Nil(t, err)
+
+	n, err = fio.Write([]byte("world"))
+	assert.Equal(t, 5, n)
+	assert.Nil(t, err)
+
+	err = fio.Sync()
+	assert.Nil(t, err)
+}
+
+func TestFileIO_Close(t *testing.T) {
+	path := filepath.Join("/tmp", "a.data")
+	fio, err := NewFileIOManger(path)
+	defer deleteFile(path)
+	assert.Nil(t, err)
+	assert.NotNil(t, fio)
+
+	n, err := fio.Write([]byte("hello"))
+	assert.Equal(t, 5, n)
+	assert.Nil(t, err)
+
+	n, err = fio.Write([]byte("world"))
+	assert.Equal(t, 5, n)
+	assert.Nil(t, err)
+
+	err = fio.Sync()
+	assert.Nil(t, err)
+
+	err = fio.Close()
+	assert.Nil(t, err)
+}
--- fio/io_manager.go
+++ fio/io_manager.go
@@ -1,1 +1,11 @@
 package fio
+
+const DataFileParm = 0644
+
+// IOManager 封装了IO操作
+type IOManager interface {
+	Read([]byte, int64) (int, error)
+	Write([]byte) (int, error)
+	Sync() error
+	Close() error
+}
